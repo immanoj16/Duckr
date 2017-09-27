5393863103938432e08a50c2843b5a61a159cb8f
immanoj16 <mmaharana14@gmail.com>
2017-09-27 21:13:03 +0530

    Add: user actions and reducers

--- app/index.js
+++ app/index.js
@@ -1,6 +1,12 @@
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
 
 import routes from './config/routes'
+import users from 'redux/modules/users'
+
+const store = createStore(users)
+console.log(store);
+console.log(store.getState());
 
 ReactDOM.render(
     routes,
