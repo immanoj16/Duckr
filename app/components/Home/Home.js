fcb131ccc091f7251046bebcf59cf50b613663f8
immanoj16 <mmaharana14@gmail.com>
2017-09-27 10:43:37 +0530

    Add: navigation bar

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,9 +1,12 @@
 import React, { PropTypes } from 'react'
 
+import { container, title, slogan } from './styles.css'
+
 const Home = (props) => {
   return (
-    <div>
-      Home
+    <div className={container}>
+      <p className={title}>{'Duckr'}</p>
+      <p className={slogan}>{'The real time, cloud based, modular, scalable, growth hack, social platform in the cloud'}</p>
     </div>
   )
 }
--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import { color } from './styles.css'
+
+import { Navigation } from 'components'
+import { container, innerContainer } from './styles.css'
 
 class MainContainer extends React.Component {
   render () {
     return (
-      <div>
-        {this.props.children}
+      <div className={container}>
+        <Navigation isAuthed={true} />
+        <div className={innerContainer}>
+          {this.props.children}
+        </div>
       </div>
     )
   }
