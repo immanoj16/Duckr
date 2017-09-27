c510c062f02a2e59917b6020007cdd3888b4717b
immanoj16 <mmaharana14@gmail.com>
2017-09-27 11:08:05 +0530

    Add: Authenticate mock

---
+++ app/components/Authenticate/Authenticate.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Authenticate = (props) => {
+  return (
+    <div>
+      Authenticate
+    </div>
+  )
+}
+
+export default Authenticate
---
+++ app/containers/Authenticate/AuthenticateContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Authenticate } from 'components'
+
+class AuthenticateContainer extends React.Component {
+  render () {
+    return (
+      <Authenticate />
+    )
+  }
+}
+
+export default AuthenticateContainer;
