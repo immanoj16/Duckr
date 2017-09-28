eae9a466a9827f694346600b1e0162d3e2a96e29
immanoj16 <mmaharana14@gmail.com>
2017-09-28 07:46:31 +0530

    Add: redux thunk

--- app/containers/Authenticate/AuthenticateContainer.js
+++ app/containers/Authenticate/AuthenticateContainer.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 import { Authenticate } from 'components'
-import auth from 'helpers/auth'
 import * as userActionCreators from 'redux/modules/users'
 
 class AuthenticateContainer extends React.Component {
@@ -11,20 +10,11 @@ class AuthenticateContainer extends React.Component {
   propTypes: {
     isFetching: PropTypes.bool.isRequired,
     error: PropTypes.string.isRequired,
-    authUser: PropTypes.func.isRequired,
-    fetchingUser: PropTypes.func.isRequired,
-    fetchingUserSuccess: PropTypes.func.isRequired,
-    fetchingUserFailure: PropTypes.func.isRequired,
+    fetchAndHandleAuthedUser: PropTypes.func.isRequired,
   }
 
   handleAuth() {
-    this.props.fetchingUser()
-    auth().then((user) => {
-      this.props.fetchingUserSuccess(user.uid, user, Date.now())
-      this.props.authUser(user.uid)
-
-    })
-    .catch((error) => this.props.fetchingUserFailure(error))
+    this.props.fetchAndHandleAuthedUser()
   }
 
   render () {
