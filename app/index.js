f53904a71c83d0414b9eb4b118c23a973a1435a7
immanoj16 <mmaharana14@gmail.com>
2017-09-29 07:38:07 +0530

    Add: dev tool extension for redux

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 
@@ -8,7 +8,10 @@ import getRoutes from './config/routes'
 import users from 'redux/modules/users'
 import { checkIfAuthed } from 'helpers/auth'
 
-const store = createStore(users, applyMiddleware(thunk))
+const store = createStore(users, compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : (f) => f
+))
 
 function checkAuth (nextState, replace) {
   if (store.getState().isFetching === true) {
