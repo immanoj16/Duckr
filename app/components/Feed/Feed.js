8a393d10dad2e78a516f6de6847c93be8b7fdfbf
immanoj16 <mmaharana14@gmail.com>
2017-10-01 08:43:09 +0530

    Add: new duck available component with some styles

--- app/components/Feed/Feed.js
+++ app/components/Feed/Feed.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 
-const Feed = (props) => {
-  console.log(props);
+import { newDuckContainer, header } from './styles.css'
+import { errorMsg } from 'sharedStyles/styles.css'
+
+
+function NewDuckAvailable ({handleClick}) {
   return (
-    <div>
-      Feeeeeed
+    <div className={newDuckContainer} onClick={handleClick}>
+      {'New Ducks Available'}
     </div>
   )
+}
+
+NewDuckAvailable.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+}
+
+const Feed = ({duckIds, error, isFetching, newDucksAvailable, resetNewDucksAvailable}) => {
+  console.log(duckIds);
+  return isFetching === true
+    ? <h1 className={header}>{'Fetching'}</h1>
+    : <div>
+        {newDucksAvailable ? <NewDuckAvailable handleClick={resetNewDucksAvailable} /> : null}
+        {duckIds.length === 0
+          ? <p className={header}>{'This is unfortunate.'}<br /> {'It appears there are no ducks yet 😞'}</p>
+          : null}
+        {duckIds.map((id) => (
+          <p>Duck Id: {id}</p>
+        ))}
+        {error ? <p className={errorMsg}>{error}</p> : null}
+      </div>
+}
+
+Feed.propTypes = {
+  duckIds: PropTypes.array.isRequired,
+  error: PropTypes.string.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  newDucksAvailable: PropTypes.bool.isRequired,
+  resetNewDucksAvailable: PropTypes.func.isRequired,
 }
 
 export default Feed
