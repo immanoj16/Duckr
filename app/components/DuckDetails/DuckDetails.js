0d9ce2ef965490db959a312ebebf7545425728fe
immanoj16 <mmaharana14@gmail.com>
2017-10-02 09:07:03 +0530

    Add: UI to duckdetails component

--- app/components/DuckDetails/DuckDetails.js
+++ app/components/DuckDetails/DuckDetails.js
@@ -4,13 +4,31 @@ import {
   mainContainer, container, content, repliesContainer,
   replyTextAreaContainer, replyTextArea } from './styles.css'
 import { subHeader, darkBtn, errorMsg } from 'sharedStyles/styles.css'
+import { DuckContainer } from 'containers'
 
-const DuckDetails = (props) => {
+const DuckDetails = ({ authedUser, duckId, isFetching, error }) => {
   return (
-    <div>
-      DuckDetails
+    <div className={mainContainer}>
+      {isFetching === true
+        ? <p className={subHeader}>{'Fetching'}</p>
+        : <div className={container}>
+            <div className={content}>
+              <DuckContainer duckId={duckId} hideLikeCount={false} hideReplyBtn={true} />
+            </div>
+            <div className={repliesContainer}>
+              Reply Section
+            </div>
+          </div>}
+      {error ? <p className={errorMsg}>{error}</p> : null}
     </div>
   )
+}
+
+DuckDetails.propTypes = {
+  authedUser: PropTypes.object.isRequired,
+  duckId: PropTypes.string.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  error: PropTypes.string.isRequired,
 }
 
 export default DuckDetails
--- app/containers/DuckDetails/DuckDetailsContainer.js
+++ app/containers/DuckDetails/DuckDetailsContainer.js
@@ -1,13 +1,32 @@
 import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
 
 import { DuckDetails } from 'components'
 
 class DuckDetailsContainer extends React.Component {
+
+  propTypes: {
+    authedUser: PropTypes.object.isRequired,
+    duckId: PropTypes.string.isRequired,
+    isFetching: PropTypes.bool.isRequired,
+    error: PropTypes.string.isRequired,
+  }
+
   render () {
+    const { authedUser, duckId, isFetching, error } = this.props
+
     return (
-      <DuckDetails />
+      <DuckDetails
+        authedUser={authedUser}
+        duckId={duckId}
+        isFetching={isFetching}
+        error={error} />
     )
   }
 }
 
-export default DuckDetailsContainer
+function mapStateToProps () {
+  return
+}
+
+export default connect(mapStateToProps)(DuckDetailsContainer)
