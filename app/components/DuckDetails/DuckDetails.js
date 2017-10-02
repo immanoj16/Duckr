9aa6681ab95878b8c2478499896a4945cb7b7707
immanoj16 <mmaharana14@gmail.com>
2017-10-02 08:53:01 +0530

    Add: duck detail component and container

---
+++ app/components/DuckDetails/DuckDetails.js
@@ -0,0 +1,16 @@
+import React, { PropTypes } from 'react'
+
+import {
+  mainContainer, container, content, repliesContainer,
+  replyTextAreaContainer, replyTextArea } from './styles.css'
+import { subHeader, darkBtn, errorMsg } from 'sharedStyles/styles.css'
+
+const DuckDetails = (props) => {
+  return (
+    <div>
+      DuckDetails
+    </div>
+  )
+}
+
+export default DuckDetails
---
+++ app/containers/DuckDetails/DuckDetailsContainer.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react'
+
+import { DuckDetails } from 'components'
+
+class DuckDetailsContainer extends React.Component {
+  render () {
+    return (
+      <DuckDetails />
+    )
+  }
+}
+
+export default DuckDetailsContainer
