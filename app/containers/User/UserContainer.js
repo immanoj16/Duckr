04068bebb88f35741002b94581d9ff72c6f21957
immanoj16 <mmaharana14@gmail.com>
2017-10-02 07:21:11 +0530

    Add: to fetch user and ducks from the firebase

---
+++ app/containers/User/UserContainer.js
@@ -0,0 +1,40 @@
+import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
+
+import { User } from 'components'
+
+class UserContainer extends React.Component {
+  render () {
+    const { noUser, name, isFetching, error, duckIds } = this.props
+    return (
+      <User
+        noUser={noUser}
+        name={name}
+        isFetching={isFetching}
+        error={error}
+        duckIds={duckIds} />
+    )
+  }
+}
+
+userContainer.propTypes = {
+  noUser: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  error: PropTypes.string.isRequired,
+  duckIds: PropTypes.array.isRequired,
+}
+
+function mapStateToProps ({users, usersDucks}, props) {
+  const specificUsersDucks = usersDucks[props.routeParams.uid]
+  const user = users[props.routeParams.uid]
+  const noUser = typeof user === 'undefined'
+  return {
+    noUser,
+    isFetching: users.isFetching || usersDucks.isFetching,
+    error: users.error || usersDucks.error,
+    duckIds: specificUsersDucks ? specificUsersDucks.duckIds : []
+  }
+}
+
+export default connect(mapStateToProps)(UserContainer)
