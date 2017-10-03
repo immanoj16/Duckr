0ac435554d3e55ad37d12012a9153b017d31a2ca
immanoj16 <mmaharana14@gmail.com>
2017-10-03 11:48:13 +0530

    Add: react-router-redux to manage route state in redux

--- app/index.js
+++ app/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { routerReducer, syncHistoryWithStore } from 'react-router-redux'
+import { hashHistory } from 'react-router'
 
 import getRoutes from './config/routes'
 import * as reducers from 'redux/modules'
 import { checkIfAuthed } from 'helpers/auth'
 
-const store = createStore(combineReducers(reducers), compose(
+const store = createStore(combineReducers({...reducers, routing: routerReducer}), compose(
   applyMiddleware(thunk),
   window.devToolsExtension ? window.devToolsExtension() : (f) => f
 ))
+
+const history = syncHistoryWithStore(hashHistory, store)
 
 function checkAuth (nextState, replace) {
   if (store.getState().users.isFetching === true) {
@@ -33,7 +37,7 @@ function checkAuth (nextState, replace) {
 
 ReactDOM.render(
     <Provider store={store}>
-      {getRoutes(checkAuth)}
+      {getRoutes(checkAuth, history)}
     </Provider>,
     document.getElementById('app')
 )
